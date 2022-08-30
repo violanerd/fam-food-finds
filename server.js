12a978e79a5e520da973b1863390b90146f44385
channchealy <sek.channchealy@yahoo.com>
2022-08-30 15:16:59 -0500

    create data model

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-// TEST
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+  });

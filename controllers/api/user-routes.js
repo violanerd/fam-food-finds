7fd44bf03158973a7f328171ad65528a2bf36240
channchealy <sek.channchealy@yahoo.com>
2022-08-31 13:00:13 -0500

    login and sigup users

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// signup
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    res.redirect("/login");
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// login
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
+});
+
+// logout
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const routes = require("./controllers");
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +11,12 @@ const sequelize = require("./config/connection");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);

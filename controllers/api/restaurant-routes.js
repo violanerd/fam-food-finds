d49a6b835b9814c1f73d40b99e1f1bdd06558169
violanerd <madalyne.cross@gmail.com>
2022-09-01 17:05:31 -0500

    extra console.log

--- controllers/api/restaurant-routes.js
+++ controllers/api/restaurant-routes.js
@@ -24,7 +24,7 @@ router.get("/", async (req, res) => {
 router.get("/:category", async (req, res) => {
   // this code allows for both "Italian" and "Burger-Joint" to be passed in
   const categoryName = req.params.category.replace("-", " ");
-  console.log(catName);
+  
   //find category id
   try{ 
     const category_id = await Category.findOne({

4742c8351db9fd28cb08d586d697bc172a83e14f
channchealy <sek.channchealy@yahoo.com>
2022-09-02 20:46:21 -0500

    homepage and comment form

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
+const restaurantRoutes = require("./restaurant-routes");
 
 router.use("/users", userRoutes);
+router.use("/restaurant", restaurantRoutes);
 
 module.exports = router;
 
--- controllers/api/restaurant-routes.js
+++ controllers/api/restaurant-routes.js
@@ -1,24 +1,35 @@
 const router = require("express").Router();
-const {Category, Restaurant, Review, RestaurantCategory, User} = require("../../models")
+const {
+  Category,
+  Restaurant,
+  Review,
+  RestaurantCategory,
+  User,
+} = require("../../models");
 
 //GET /api/restaurant
 //get all restaurants
 router.get("/", async (req, res) => {
-    try {
-      const restaurantData = await Restaurant.findAll({
-        include: [{model: Category,
-        attributes: ["category_name"]}, {model: Review,
-        attributes: ["id", "review_text", "user_id"],
-      include: {model: User, attributes: ["username"]}}]
-      })
-      res.status(200).json(restaurantData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  
-  });
+  try {
+    const restaurantData = await Restaurant.findAll({
+      include: [
+        { model: Category, attributes: ["category_name"] },
+        {
+          model: Review,
+          attributes: ["id", "review_text", "user_id"],
+          include: { model: User, attributes: ["username"] },
+        },
+      ],
+    });
+    res.status(200).json(restaurantData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  
-
+// get a restaurant
+router.get("/:id", async (req, res) => {
+  res.render("restaurant");
+});
 
 module.exports = router;

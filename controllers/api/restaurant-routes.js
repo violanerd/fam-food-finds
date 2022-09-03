6a5ff9a69b6ffa6d53a6013e823e6810e26c4aec
Doug Red <douglasred63@yahoo.com>
2022-09-03 13:59:50 -0500

    added get all restaurants route

--- controllers/api/restaurant-routes.js
+++ controllers/api/restaurant-routes.js
@@ -49,19 +49,100 @@ router.get("/:category", async (req, res) => {
 //   }
 // })
 
-// POST create new restaurant /api/restaurant/
-router.post("/", async (req, res) => {
-  try {
-    const restaurantData = await Restaurant.create({
-      restaurant_name: req.body.restaurant_name,
-      restaurant_url: req.body.restaurant_url,
-      user_id: req.body.user_id,
+// Get All Restaurants
+router.get("/", (req, res) => {
+  console.log("======================");
+  Restaurant.findAll({
+    attributes: ["id", "restaurant_name", "restaurant_url", "user_id"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: Review,
+        attributes: [
+          "id",
+          "review_text",
+          "rating",
+          "user_id",
+          "restaurant_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    res.status(200).json(restaurantData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
+
+// Get a Restaurant
+// router.get("/:id", (req, res) => {
+//   Post.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: [
+//       "id",
+//       "post_url",
+//       "title",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//         ),
+//         "vote_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: "No post found with this id" });
+//         return;
+//       }
+//       res.json(dbPostData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// POST create new restaurant /api/restaurant/
+// router.post("/", (req, res) => {
+//   Restaurant.create({
+//     restaurant_name: req.body.restaurant_name,
+//     restaurant_url: req.body.restaurant_url,
+//     user_id: req.session.user_id,
+//   })
+//     .then((dbRestaurantData) => res.json(dbRestaurantData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 // PUT update restaurant /api/restaurant/1
 router.put("/:id", async (req, res) => {

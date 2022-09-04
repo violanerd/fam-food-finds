d90625a0ea3631f874a35dde389061c7d98a1f55
channchealy <sek.channchealy@yahoo.com>
2022-09-04 16:51:54 -0500

    add restaurant description

--- public/javascript/edit-restaurant.js
+++ public/javascript/edit-restaurant.js
@@ -1,23 +1,25 @@
 
 const newFormHandler = async function (event) {
-    event.preventDefault();
-  
-    const restaurant_name = document.querySelector('input[name="restaurant-name"]').value;
-    const restaurant_url = document.querySelector('input[name="restaurant-url"]').value;
-  
-    await fetch("/api/post", {
-      method: "POST",
-      body: JSON.stringify({
-        postTitle,
-        postContent,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  
-    document.location.replace("/dashboard");
-  };
-  
-  
-  document
-    .querySelector("#edit-restaurant-form")
-    .addEventListener("submit", newFormHandler);
+  event.preventDefault();
+
+  const restaurant_name = document.querySelector('input[name="restaurant-name"]').value;
+  const restaurant_url = document.querySelector('input[name="restaurant-url"]').value;
+  const restaurant_description = document.querySelector('textarea[name="restaurant-description"]').value;
+
+  await fetch("/api/post", {
+    method: "POST",
+    body: JSON.stringify({
+      restaurant_name,
+      restaurant_url,
+      restaurant_description,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  document.location.replace("/dashboard");
+};
+
+
+document
+  .querySelector("#btn-edit")
+  .addEventListener("click", newFormHandler);
